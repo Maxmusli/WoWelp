0455887c60532e687c838ca5becb910641fea70a
max <maxmus2223@gmail.com>
2019-11-08 20:07:26 -0800

    add hover drop down effect to sub nav categories

--- frontend/components/nav_bar/sub_nav_categories.jsx
+++ frontend/components/nav_bar/sub_nav_categories.jsx
@@ -36,17 +36,22 @@ export default class SubNavCategories extends Component {
       <div className="sub-nav-categories">
         <div className="wrapper">
           <div className="container">
-            <div className="category">
-              < a href="" >
+            <div className="dropdown">
+              <a href="" className="dropbtn">
                 <div className="header-wrapper">
                   <i className="fas fa-utensils"></i>
                   <h3>Restaurants</h3>
                   <i className="fas fa-caret-down"></i>
                 </div>
               </a >
+              <div className="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
             </div>
             
-            <div className="category">
+            <div className="dropdown">
               <a href="">
                 <div className="header-wrapper">
                   <i className="fas fa-hammer"></i>
@@ -54,8 +59,13 @@ export default class SubNavCategories extends Component {
                   <i className="fas fa-caret-down"></i>
                 </div>
               </a>
+              <div className="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
             </div>
-            <div className="category">
+            <div className="dropdown">
               <a href="">
                 <div className="header-wrapper">
                   <i className="fas fa-chess-rook"></i>
@@ -63,14 +73,24 @@ export default class SubNavCategories extends Component {
                   <i className="fas fa-caret-down"></i>
                 </div>
               </a>
+              <div className="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
             </div>
-            <div className="category">
+            <div className="dropdown">
               <a href="">
                 <div className="header-wrapper">
                   <h3>More</h3>
                   <i className="fas fa-caret-down"></i>
                 </div>
               </a>
+              <div className="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
             </div>
 
             <div className="blink">
