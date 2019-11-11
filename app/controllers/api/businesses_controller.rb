f4803070b9c83c82402be881b12025e084c1b4c0
max <maxmus2223@gmail.com>
2019-11-10 16:53:43 -0800

    attempt to fix filter funcitonality

--- app/controllers/api/businesses_controller.rb
+++ app/controllers/api/businesses_controller.rb
@@ -2,12 +2,17 @@ class Api::BusinessesController < ApplicationController
   before_action :require_logged_in, only: [:create]
 
   def index
-    near = params[:filters][:near]
-    find = params[:filters][:find]
-
-    if find
-
     
+    # near = params[:filters][:near]
+    # find = params[:filters][:find]
+    
+    # if near != "" && find != ""
+    #   @businesses = Business.near_location(near)
+    #   @matches = Business.find_business(find)
+    #   @businesses = Business.select { |business| @matches.include?(business) }
+    # end
+
+    @businesses = Business.all
     
     render :index
   end
