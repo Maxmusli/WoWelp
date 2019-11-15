c1afd1e6912287fece67e6da1f064b613d43cc3b
max <maxmus2223@gmail.com>
2019-11-15 07:14:23 -0800

    add css styling to review form and fix review form nav bar bug

--- frontend/components/reviews/review_form_container.js
+++ frontend/components/reviews/review_form_container.js
@@ -1,8 +1,10 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import { createReview } from '../../actions/review_actions';
 import ReviewForm from './review_form';
 import { selectBusiness } from '../../reducers/selectors';
-import { fetchBusiness } from '../../actions/business_actions'
+import { fetchBusiness } from '../../actions/business_actions';
+import { logout } from '../../actions/session_action'
 
 
 
@@ -13,15 +15,17 @@ const mapStateToProps = (state, ownProps) => {
   return ({
     businessId,
     business,
+    currentUser: state.entities.users[state.session.id],
   })
 };
 
 const mapDispatchToProps = dispatch => ({
   createReview: review => dispatch(createReview(review)),
-  fetchBusiness: id => dispatch(fetchBusiness(id))
+  fetchBusiness: id => dispatch(fetchBusiness(id)),
+  logout: () => dispatch(logout()),
 });
 
-export default connect(
+export default withRouter(connect(
   mapStateToProps,
   mapDispatchToProps
-)(ReviewForm);
+)(ReviewForm));
