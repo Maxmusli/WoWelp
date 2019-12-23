87dbce37d0a01700e7809f3e3d1e600a13a6419c
max <maxmus2223@gmail.com>
2019-12-22 18:46:49 -0800

    attempt to fix map bugs

--- frontend/components/map/business_map.jsx
+++ frontend/components/map/business_map.jsx
@@ -7,19 +7,19 @@ import MapMarker from '../../util/map_marker';
 //   lng: latLng.lng()
 // });
 
-// const mapOptions = {
-//   center: {
-//     lat: 37.773972,
-//     lng: -122.431297
-//   }, // SF coords
-//   zoom: 11
-// }
+let mapOptions = {
+  center: {
+    lat: 37.773972,
+    lng: -122.431297
+  }, // SF coords
+  zoom: 11
+}
 
 class BusinessMap extends Component {
   componentDidMount() {
     let center;
     let zoom;
-
+    
     if (this.props.businesses.length >= 1) {
       
       let lats = this.props.businesses.map(business => {business.lat});
@@ -37,10 +37,10 @@ class BusinessMap extends Component {
       zoom = 15;
     }
 
-    const mapOptions = {
-      center: center,
-      zoom: zoom,
-    }
+    // const mapOptions = {
+    //   center: center,
+    //   zoom: zoom,
+    // }
     
 
     const map = this.refs.map;
@@ -50,6 +50,31 @@ class BusinessMap extends Component {
   }
 
   componentDidUpdate() {
+    let center;
+    let zoom;
+
+    if (this.props.businesses.length >= 1) {
+      
+      let lats = this.props.businesses.map(business => { business.lat });
+      let sumLats = lats.reduce((acc, ele) => acc + ele);
+      let avgLat = sumLats / this.props.businesses.length;
+
+      let lngs = this.props.businesses.map(business => { business.lng });
+      let sumLngs = lngs.reduce((acc, ele) => acc + ele);
+      let avgLng = sumLngs / this.props.businesses.length;
+
+      center = { lat: avgLat, lng: avgLng };
+      zoom = 11;
+    } else if (this.props.business) {
+      center = { lat: this.props.business.lat, lng: this.props.business.lng };
+      zoom = 15;
+    }
+
+    mapOptions = {
+      center: center,
+      zoom: zoom,
+    }
+
     this.MapMarker.updateMarkers(this.props.businesses);
   }
 
