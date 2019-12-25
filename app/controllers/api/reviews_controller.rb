5883d0f911fe060f7dab1a8fab15396ba8568aec
max <maxmus2223@gmail.com>
2019-12-25 16:20:36 -0500

    add edit and update in reviews controller

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -11,6 +11,20 @@ class Api::ReviewsController < ApplicationController
     end
   end
 
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = current_user.reviews.find(params[:id])
+
+    if @review.update
+      render :show
+    else
+      render json: @review, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @review = Review.find(params[:id])
     @review.destroy
