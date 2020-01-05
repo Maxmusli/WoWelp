3f5533e01dae10553d368bcc7c78bd842573c605
max <maxmus2223@gmail.com>
2020-01-04 17:29:17 -0800

    add modal actions and reducer

--- frontend/reducers/root.js
+++ frontend/reducers/root.js
@@ -3,14 +3,14 @@ import entitiesReducer from './entities';
 import sessionReducer from './session_reducer';
 import errorsReducer from './errors_reducer';
 import filtersReducer from './filters_reducer';
-// import uiReducer from './ui_reducer';
+import uiReducer from './ui_reducer';
 
 const rootReducer = combineReducers({
   entities: entitiesReducer,
   session: sessionReducer,
   errors: errorsReducer,
   filters: filtersReducer,
-  // ui: uiReducer,
+  ui: uiReducer,
 })
 
 export default rootReducer;
