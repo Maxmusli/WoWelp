a4b8d36867df5fdf18204c27dbccd095cb79b771
max <maxmus2223@gmail.com>
2020-02-17 15:26:30 -0800

    add lifecycle methods using hooks for user info on user show page

--- frontend/components/user/user_show.jsx
+++ frontend/components/user/user_show.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import UserShowNavContainer from '../nav_bar/user_show_nav_container';
 import Footer from '../nav_bar/footer_nav';
 
-function UserShowPage() {
+function UserShowPage(props) {
+  
+  useEffect(() => {
+    props.fetchCurrentUser(props.match.params.userId)
+  }, [])
   
   return (
     <div>
@@ -10,6 +14,29 @@ function UserShowPage() {
         <UserShowNavContainer />
       </div>
       <div className="bar-2"></div>
+      <div>
+        <div className="user-top-section-wrapper">
+          <div className="top-content-container">
+            <div className="profile-avatar">
+
+            </div>
+            <div className="top-content">
+              <div className="avator-dummy"></div>
+              <div className="profile-info">
+                <div>
+                  {props.users[0].fname}
+                </div>
+              </div>
+              <div className="profile-action">
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="user-bottom-section-wrapper">
+
+        </div>
+      </div>
       <div className="bar-2"></div>
       <div>
         <Footer />
