2da0330cff895a62d13a3f4777ce4af09a5e696a
max <maxmus2223@gmail.com>
2020-02-17 10:55:33 -0800

    add user show page url

--- frontend/components/user/user_show.jsx
+++ frontend/components/user/user_show.jsx
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
+import SubNavContainer from '../nav_bar/sub_nav_container';
+import Footer from '../nav_bar/footer_nav';
 
 function UserShowPage() {
   
+  return (
+    <div>
+      <div>
+        <SubNavContainer />
+      </div>
+      <div className="bar-2"></div>
+      <div className="bar-2"></div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  )
 }
 
 export default UserShowPage
