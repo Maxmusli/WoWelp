908b5b005da67a51b192f5b6dd31c3bc9d7905bd
max <maxmus2223@gmail.com>
2020-02-23 17:51:14 -0800

    add loader reducer to ui reducer

--- frontend/reducers/loader_reducer.js
+++ frontend/reducers/loader_reducer.js
@@ -10,9 +10,20 @@ const loaderState = {
   showLoader: false
 }
 
-const loaderReducer = (state = {}, action) => {
+const loaderReducer = (state = loaderState, action) => {
   Object.freeze(state)
   switch(action.type) {
-    
+    case LOAD_BUSINESSES:
+      return Object.assign({}, { indexLoader: true })
+    case LOAD_BUSINESS:
+      return Object.assign({}, { showLoader: true })
+    case RECEIVE_BUSINESSES:
+      return Object.assign({}, { indexLoader: false })
+    case RECEIVE_BUSINESS:
+      return Object.assign({}, { showLoader: false })
+    default:
+      return state;
   }
 }
+
+export default loaderReducer;
--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
-
 import ModalReducer from "./modal_reducer";
+import LoaderReducer from './loader_reducer';
 
 export default combineReducers({
-  modal: ModalReducer
+  modal: ModalReducer,
+  loader: LoaderReducer
 });
